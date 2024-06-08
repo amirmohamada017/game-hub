d972c847e8c7a5a6abc522fb4f4ceaf780f5040d
Amirmohamad <amirmohamada017@gmail.com>
2024-06-09 01:08:39 +0330

    refactor fetch games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
-  baseURL: 'https://api.rawg.io/api',
+  baseURL: "https://api.rawg.io/api",
   params: {
-    key: 'c86dde6141594e439bb800590fa17c06'
-  }
+    key: "c86dde6141594e439bb800590fa17c06",
+  },
 });
