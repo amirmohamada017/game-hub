a678a612ea6ac4715b60c032cd716fe1ff83b4f6
Amirmohamad <amirmohamada017@gmail.com>
2024-06-10 01:47:50 +0330

    build game trailer component

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,21 @@
+import useTrailer from "../hooks/useTrailers";
+
+interface Props {
+  gameId: number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, error, isLoading } = useTrailer(gameId);
+
+  if (isLoading) return null;
+
+  if (error) throw error;
+
+  const first = data?.results[0];
+
+  if (!first) return null;
+
+  return <video src={first.data[480]} poster={first.preview} controls />;
+};
+
+export default GameTrailer;
